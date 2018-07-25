245436b08a069172d9a7489084e83ea4b4a1e8de
Francheska <francheska-guzman@users.noreply.github.com>
2018-07-25 10:21:25 -0400

    Adding Hero.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Hero from './components/Hero';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+  } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div id="app-wrapper">
+         <Switch>
+            <Route exact path="/" component={() => ( <Hero /> )} />
+            <Redirect from="*" to="/" />
+         </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ front-end/src/components/Hero.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hero extends Component {
+  render() {
+    return (
+      <div id='hero-wrapper'>
+        <h1>Francheska Guzman</h1>
+      </div>
+    );
+  }
+}
+
+export default Hero;
