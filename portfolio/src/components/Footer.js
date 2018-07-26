8fbad63eaf5b72f82abd6aa169a7e67441dca4f9
Francheska <francheska-guzman@users.noreply.github.com>
2018-07-26 17:19:21 -0400

    Moving images.

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
+import GitHub from "../images/icons/github-icon.png";
+import Email from "../images/icons/email-icon.png";
+import LinkedIn from "../images/icons/linkedin-icon.png";
+
 class Footer extends Component {
   render() {
     return (
       <div className='footer'>
         <h6>Handmade by <a href="https://github.com/francheska-guzman" target="_black">Francheska Guzman</a>.</h6>
-        <a href="https://github.com/francheska-guzman" target="_black"><img className="footer-icon" src="/images/github-icon.png" alt="GitHub" /></a>
-        <a href="mailto:gffrancheska@gmail.com"><img className="footer-icon" src="/images/email-icon.png" alt="Email: gffrancheska@gmail.com" /></a>
-        <a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="footer-icon" src="/images/linkedin-icon.png" alt="LinkedIn" /></a>
+        <a href="https://github.com/francheska-guzman" target="_black"><img className="footer-icon" src={GitHub} alt="GitHub" /></a>
+        <a href="mailto:gffrancheska@gmail.com"><img className="footer-icon" src={Email} alt="Email" /></a>
+        <a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="footer-icon" src={LinkedIn} alt="LinkedIn" /></a>
       </div>
     );
   }
