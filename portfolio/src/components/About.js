36d0b060905c7447f0e234fa19c2148c2917645e
Francheska <francheska-guzman@users.noreply.github.com>
2018-07-28 23:20:38 -0400

    Updating

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -5,8 +5,8 @@ class About extends Component {
     return (
       <div className='wrapper about'>
         <h2>About</h2>
-        <p>Francheska is a passionate web developer based in New York.</p>
-        <p>Since 2014, she is an active member of <a href="https://www.utest.com/" target="_black">uTest</a>, a crowdsourced software testing community. Francheska has tested web and native applications by performing localization and exploratory testing across modern platforms and devices. </p>
+        <p>Francheska is a passionate web developer based in New York. Her primary role is working as a Jr. Front End Programmer at the <a href="https://www.nyfa.edu/" target="_black">New York Film Academy — School of Film and Acting</a>.</p>
+        <p>Since 2014, she is an active member of <a href="https://www.utest.com/" target="_black">uTest</a>, a crowdsourced software testing community. Francheska has tested web and native applications by performing localization and exploratory testing, as well as executing test cases across modern platforms and devices. </p>
         <p>Early 2018, Francheska joined <a href="http://www.secondlanguagedesign.com/" target="_black">Second Language Design</a>, a boutique NYC-based creative agency where she has contributed in building WordPress websites for clients such as <a href="https://painfreecats.org/" target="_black">The North Carolina State University’s College of Veterinary Medicine</a> and <a href="http://aprpc.com/" target="_black">Acupuncture Remedies</a>.</p>
         <p>Her experience working with technologies such as HTML, CSS, SASS/SCSS, JavaScript, jQuery, React, Node.js, Express.js, PostgreSQL, API's, GitHub and the WordPress stack, makes her confident to work with both front-end and back-end development.</p>
       </div>
