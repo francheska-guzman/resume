aca37144880c2d233bc7b540d3c431afe9f1436f
Francheska <francheska-guzman@users.noreply.github.com>
2018-07-31 07:45:48 -0400

    Add picture.

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import picture from "../assets/images/about-picture.jpg";
+
 class About extends Component {
   render() {
     return (
       <div className='wrapper about'>
         <h2>About</h2>
-        <p>Francheska is a passionate web developer based in New York. Her primary role is working as a Jr. Front End Programmer at the <a href="https://www.nyfa.edu/" target="_black">New York Film Academy — School of Film and Acting</a>.</p>
+        <div className="flex">
+	        <img src={picture} alt="Francheska Guzman" />
+	        <p className="introduction">Francheska is a passionate web developer based in New York. Her primary role is working as a Jr. Front End Programmer at the <a href="https://www.nyfa.edu/" target="_black">New York Film Academy — School of Film and Acting</a>.</p>
+	    </div>
         <p>Since 2014, she is an active member of <a href="https://www.utest.com/" target="_black">uTest</a>, a crowdsourced software testing community. Francheska has tested web and native applications by performing localization and exploratory testing, as well as executing test cases across modern platforms and devices. </p>
         <p>Early 2018, Francheska joined <a href="http://www.secondlanguagedesign.com/" target="_black">Second Language Design</a>, a boutique NYC-based creative agency where she has contributed in building WordPress websites for clients such as <a href="https://painfreecats.org/" target="_black">The North Carolina State University’s College of Veterinary Medicine</a> and <a href="http://aprpc.com/" target="_black">Acupuncture Remedies</a>.</p>
         <p>Her experience working with technologies such as HTML, CSS, SASS/SCSS, JavaScript, jQuery, React, Node.js, Express.js, PostgreSQL, API's, GitHub and the WordPress stack, makes her confident to work with both front-end and back-end development.</p>
